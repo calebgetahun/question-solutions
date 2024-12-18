b5063b5e5ea54db95831d1b1c5ec19535d2c140d
Caleb Getahun <calebgetahun011@gmail.com>
2024-12-18 17:53:13 -0500

    valid palindrome 2 desc + TC/SC

--- Leetcode/String/ValidPalindrome2.py
+++ Leetcode/String/ValidPalindrome2.py
@@ -1,3 +1,30 @@
+"""
+Problem description:
+https://leetcode.com/problems/valid-palindrome-ii/
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+"""
+
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         n = len(s)
@@ -18,8 +45,14 @@ class Solution:
             end -= 1
         return True
     
-
+#test cases
 testCases = ['aba', 'eceec', 'zrtetruz', 'aaaa', 'frghuhewrurwehihgrf', " ' ' kd"]
 solution = Solution()
 for word in testCases:
     print(solution.validPalindrome(word))
+
+"""
+TC: O(n) since we are iterating through the string once until we encounter mismatched strings. When this occurs we check 2 instances
+of the problem space by looking at the different strings that could be our answer if we deleted one char or the other
+SC: O(1) since we are storing the information of 2 pointers throughout the implementation
+"""
