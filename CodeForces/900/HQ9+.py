6f3e1d6717522e095ee836b13d9898f038560056
Caleb Getahun <calebgetahun011@gmail.com>
2024-12-24 12:40:38 -0500

    kefa and first steps

---
+++ CodeForces/900/HQ9+.py
@@ -0,0 +1,10 @@
+def hq9plus(s: str):
+    commands = {"H", "Q", "9"}
+    for char in s:
+        if char in commands:
+            return("YES")
+    return("NO")
+
+if __name__ == "__main__":
+    command = input()
+    print(hq9plus(command))
---
+++ CodeForces/900/kefa_and_first_steps.py
@@ -0,0 +1,18 @@
+def increasing_money(nums: list):
+    increasing = 1
+    maxSub = 1
+    for i in range(1, len(nums)):
+        if nums[i] >= nums[i - 1]:
+            increasing += 1
+        else:
+            increasing = 1
+        maxSub = max(maxSub, increasing)
+
+    return maxSub
+
+if __name__ == "__main__":
+    n = int(input())
+
+    nums = list(map(int, input().split(" ")))
+    
+    print(increasing_money(nums))
