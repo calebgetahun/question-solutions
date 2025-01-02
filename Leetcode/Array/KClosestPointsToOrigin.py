9a1b5fcaf45e3e7b2674a05bae3a59dee855c4aa
Caleb Getahun <calebgetahun011@gmail.com>
2025-01-01 22:16:30 -0500

    refined approach to k closest points to incorporate max heap for asymptotically better complexity

--- Leetcode/Array/KClosestPointsToOrigin.py
+++ Leetcode/Array/KClosestPointsToOrigin.py
@@ -1,19 +1,20 @@
+from typing import List
 import heapq
 
 class Solution:
-    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
-        dist_heap = []
-
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
         for point in points:
-            dist = (point[0]**2 + point[1]**2)**0.5
-            dist_heap.append((dist, point))
+            dist = -(point[0]**2 + point[1]**2)
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, (dist, point))
 
-        heapq.heapify(dist_heap)
+            else:
+                heapq.heappushpop(max_heap, (dist, point))
+
         final = []
-
-        for _ in range(k):
-            res = heapq.heappop(dist_heap)[1]
-            final.append(res)
+        for dist, point in max_heap:
+            final.append(point)
         return final
 
 if __name__ == "__main__":
@@ -22,5 +23,5 @@ if __name__ == "__main__":
     k = 2
     print(sol.kClosest(points, k))
 
-# TC: O(KlogN)
+# TC: O(NlogK)
 # SC: O(N)
