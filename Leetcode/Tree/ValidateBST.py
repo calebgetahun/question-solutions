d4cb9e92c828ee9156281bac2da1a5758663e2aa
Caleb Getahun <calebgetahun011@gmail.com>
2025-01-17 21:44:34 -0500

    using ranges for values

--- Leetcode/Tree/ValidateBST.py
+++ Leetcode/Tree/ValidateBST.py
@@ -28,11 +28,28 @@ class Solution:
                 return False
         return True
     
+    def isValidBSTUsingRanges(self, root: Optional[TreeNode]) -> bool:
+        def helper(root, left, right):
+            if not root:
+                return True
+
+            if root.val <= left or root.val >= right:
+                return False
+            
+            left = helper(root.left, left, root.val)
+            right = helper(root.right, root.val, right)
+        
+            return left and right
+
+        left, right = float('-inf'), float('inf')
+        return helper(root, left, right)
+    
 
 if __name__ == "__main__":
     sol = Solution()
     tree = TreeNode(5, TreeNode(4), TreeNode(6, TreeNode(3), TreeNode(7)))
     print(sol.isValidBST(tree))
+    print(sol.isValidBSTUsingRanges(tree))
 
 # TC: O(N) where N is the number of nodes in our tree
 # SC: O(N + h) = O(N) since we store all our nodes values in a list for comparison
