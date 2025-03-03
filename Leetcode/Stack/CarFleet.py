8b05801ce1061a04ebca253f7a07b9bae9792a08
Caleb Getahun <calebgetahun011@gmail.com>
2025-03-02 19:31:56 -0500

    car fleet

--- Leetcode/Stack/CarFleet.py
+++ Leetcode/Stack/CarFleet.py
@@ -1,12 +1,33 @@
 from typing import List
 
 class Solution:
-    def carFleet(self, target: int, position: List[int], speed: List[int]):
-        position_speed = []
-        n = len(position)
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = list(zip(position, speed))
+        pairs.sort(key=lambda pair: pair[0], reverse=True)
 
-        for i in range(n):
-            position_speed.append((position[i], speed[i]))
+        dist = []
+        for i in range(len(pairs)):
+            distance_to_target = (target - pairs[i][0])/pairs[i][1]
+            dist.append(distance_to_target)
         
-        position_speed.sort(key=lambda x:x[0], reversed=True)
-        
+        stack = [dist[0]]
+        for i in range(1, len(dist)):
+            popped = stack.pop()
+            if dist[i] <= popped:
+                stack.append(max(dist[i], popped))
+            else:
+                stack.append(popped)
+                stack.append(dist[i])
+
+        return len(stack)                
+
+if __name__ == "__main__":
+    sol = Solution()
+    target = 12
+    position = [10,8,0,5,3]
+    speed = [2,4,1,1,3]
+
+    print(sol.carFleet(target, position, speed))
+
+# TC: O(NlogN), as sorting is the dominant term
+# SC: O(N), to create our stack and the distance array
