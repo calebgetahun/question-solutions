1a24f11da3bed7b19694e25a6cf18e88f80f6c83
Caleb Getahun <calebgetahun011@gmail.com>
2025-04-06 19:55:21 -0400

    contains duplicate

---
+++ Leetcode/Array/ContainsDuplicate.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        
+        return False
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
+# TC: O(N)
+# SC: O(N)
